367ed057400ea4012d0631f085f2c2698ab09d7d
Yugo <yugosakamoto0717@gmail.com>
2022-07-06 12:56:15 -0300

    testando deploy

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ dotenv.config();
 const server = express();
 server.use(cors());
 server.use(express.json());
+server.get("/", (req, res) => res.send("Testando deploy"));
 
 const PORT = process.env.PORT;
 server.listen(PORT, () => console.log("Servidor iniciado na porta " + PORT));
