385083eb169c843ad55a485cbaf964c1bb89c471
Yugo <yugosakamoto0717@gmail.com>
2022-07-06 13:04:08 -0300

    testando o deploy 2

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import db from '../src/db/mongodb.js';
 
 dotenv.config();
 
@@ -8,6 +9,13 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 server.get("/", (req, res) => res.send("Testando deploy"));
+server.post("/", async (req, res) => {
+    try {
+        const userTest = req.body;
+        await db.collection("users").insertOne({ userTest });
+        res.status(200).send("Deploy funcionou!!");
+    }
+})
 
 const PORT = process.env.PORT;
 server.listen(PORT, () => console.log("Servidor iniciado na porta " + PORT));
