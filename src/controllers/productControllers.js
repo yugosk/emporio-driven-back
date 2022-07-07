65e80fb253266e879fb46f98e83d2ca5a39c78cc
Yugo <yugosakamoto0717@gmail.com>
2022-07-07 18:14:10 -0300

    Implements the GET requisitions for the different categories of products in the routes '/:category'

--- src/controllers/productControllers.js
+++ src/controllers/productControllers.js
@@ -8,3 +8,16 @@ export async function getProducts(req, res) {
     res.status(500).send("Houve um erro ao se conectar ao servidor");
   }
 }
+
+export async function getProductsByCategory(req, res) {
+  const idCategory = req.params.category;
+  try {
+    const productList = await db
+      .collection("products")
+      .find({ category: idCategory })
+      .toArray();
+    res.status(200).send(productList);
+  } catch (error) {
+    res.status(500).send("Houve um erro ao se conectar ao servidor");
+  }
+}
