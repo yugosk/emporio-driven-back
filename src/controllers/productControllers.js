041025d08d0a4757958212e8aa9e335847087add
TiaraOliveira <tiaragomes@yahoo.com.br>
2022-07-07 22:24:41 -0300

    implements renderProduct

--- src/controllers/productControllers.js
+++ src/controllers/productControllers.js
@@ -8,3 +8,14 @@ export async function getProducts(req, res) {
     res.status(500).send("Houve um erro ao se conectar ao servidor");
   }
 }
+
+
+export async function renderProduct(req, res) {
+  const name = req.params.name;
+  try {
+    const productList = await db.collection("products").find({name: name}).toArray();
+    res.status(200).send(productList);
+  } catch (error) {
+    res.status(500).send("Houve um erro ao se conectar ao servidor");
+  }
+}
--- src/routers/productRouter.js
+++ src/routers/productRouter.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
-import { getProducts } from "../controllers/productControllers.js";
+import { getProducts, renderProduct } from "../controllers/productControllers.js";
 
 const productRouter = Router();
 
+
 productRouter.get("/", getProducts);
+
+productRouter.get("/produto/:name", renderProduct);
 
 export default productRouter;
